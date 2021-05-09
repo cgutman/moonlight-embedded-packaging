807a410a265dfbdab7a69806944ba45bd43d9b40
Cameron Gutman <aicommander@gmail.com>
2021-05-09 14:12:31 +0000

    Add libp8-platform-dev for libcec

--- scripts/install-base-deps.sh
+++ scripts/install-base-deps.sh
@@ -1,4 +1,4 @@
 set -e
 export DEBIAN_FRONTEND=noninteractive
 apt-get update
-apt-get install -y curl g++ make devscripts fakeroot debhelper git nasm libssl-dev libopus-dev libasound2-dev libudev-dev libavahi-client-dev libcurl4-openssl-dev libevdev-dev libexpat1-dev libpulse-dev uuid-dev libenet-dev libcec-dev cmake quilt
+apt-get install -y curl g++ make devscripts fakeroot debhelper git nasm libssl-dev libopus-dev libasound2-dev libudev-dev libavahi-client-dev libcurl4-openssl-dev libevdev-dev libexpat1-dev libpulse-dev uuid-dev libenet-dev libcec-dev cmake quilt libp8-platform-dev
