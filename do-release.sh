d5cb13837d9636e6a949a85e341244f90fca535c
Cameron Gutman <aicommander@gmail.com>
2021-07-24 23:50:36 +0000

    Add do-release.sh script

---
+++ do-release.sh
@@ -0,0 +1,21 @@
+fail()
+{
+	echo "$1" 1>&2
+	exit 1
+}
+
+git diff-index --quiet HEAD -- || fail "Release builds must not have unstaged changes!"
+
+set -e
+
+# Ensure build images are pushed to DockerHub
+./push-images.sh
+
+# Build the packages
+./build-rpi.sh
+
+# Push the moonlight-embedded packages to Cloudsmith repos
+cloudsmith push deb moonlight-game-streaming/moonlight-embedded/raspbian/buster out_rpi-buster/moonlight-embedded_*.deb
+
+# Push the moonlight-embedded-dbgsym packages to Cloudsmith repos
+cloudsmith push deb moonlight-game-streaming/moonlight-embedded/raspbian/buster out_rpi-buster/moonlight-embedded-dbgsym_*.deb
